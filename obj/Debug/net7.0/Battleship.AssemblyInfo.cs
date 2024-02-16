5347315ee825a19ccd13b4c6007612fb223e0938
moshesilver <moshetzvi03@gmail.com>
2024-02-15 21:42:54 -0500

    stuff

--- obj/Debug/net7.0/Battleship.AssemblyInfo.cs
+++ obj/Debug/net7.0/Battleship.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35cb74e13f0d914411a8e2b017c213eeb7755e1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd348bcb4ce33de7839b25a9f917d589e3d4409")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
