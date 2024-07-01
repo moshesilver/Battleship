a86fc6ae649fd433a15b8c230c371d37954e44cd
moshesilver <moshetzvi03@gmail.com>
2024-06-30 22:37:14 -0400

    CommonConsoleAppMethods - library

--- obj/Debug/net7.0/Battleship.AssemblyInfo.cs
+++ obj/Debug/net7.0/Battleship.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15d16c46e1b100e50e95f8430e016273b905d21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0c07ce2f3864151de3a14dcd917037308ea274")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
